764a9e212b9b51a8e0a536734c8d327a15dcfccd
Pierre-Alexandre Balland <p.balland@uu.nl>
2020-10-19 10:58:54 +0200

    up

--- teaching/quea/lab.R
+++ teaching/quea/lab.R
@@ -17,19 +17,24 @@ head(NL)
 #install.packages("igraph")
 library (igraph)
 
+# graph theory: vertices = nodes and edges = links 
 g <- graph_from_data_frame(d=EL, vertices=NL, directed=FALSE)
 
 plot (g)
 
 # D3 network (interactive)
-
 EL2 = EL[, 1:2]
-
 head(EL2)
 
 #install.packages("networkD3")
 library (networkD3)
 simpleNetwork(EL2)
+
+# compute network metrics
+df = data.frame (name = V(g)$name,
+                 cent = degree (g), 
+                 betw = betweenness(g))
+
 
 # network model 
 
@@ -47,16 +52,20 @@ M2000 = subset (M, dec == 2000)
 M2000 = M2000[, c("Cbsa.Name", "NBER.Sub.Cat.Name", "pat.count")]
 
 # Transform the data into an adjacency matrix (using "get.matrix")
-M2000 = get.matrix (M2000)
+M2000 = get.matrix (M2000) # from EconGeo
 
 # Compute the co-occurrences of technologies (co.occurrence)
 c = co.occurrence (t(M2000))
+c2 = co.occurrence (M2000)
 
 # Compute relatedness between technologies and make the matrix binary
+# normalize co-occurences 
 r = relatedness (c)
+r2 = get.list(relatedness (c2))
 
 # plot 
 g = graph_from_adjacency_matrix(r, weighted=TRUE, mode="undirected", diag=FALSE)
+#par(mar=c(0,0,0,0))
 plot (g)
 
 r[r<1] = 0
