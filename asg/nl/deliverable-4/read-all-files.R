0297a0ff9e98e6ddd37f4de003c99cdbefaa6026
Pierre-Alexandre Balland <p.balland@uu.nl>
2024-02-17 05:41:29 -0600

    up

---
+++ asg/nl/deliverable-4/read-all-files.R
@@ -0,0 +1,6 @@
+
+
+setwd("/Users/pierre-alex/Dropbox/1-asg/1-production/3-projects/4-dutch-regions/3-outputs/deliverable-4")
+# List all CSV files in the folder
+files <- list.files(pattern = "\\.html$", full.names = TRUE, recursive = T)
+
