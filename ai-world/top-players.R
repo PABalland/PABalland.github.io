45939e238f2761542fd51d18f2113455646e860b
Pierre-Alexandre Balland <p.balland@uu.nl>
2024-10-12 11:09:59 +0200

    up

--- ai-world/top-players.R
+++ ai-world/top-players.R
@@ -13,12 +13,19 @@ df$color[df$country == "Israel"] = "green"
 df$parent2[df$country == "Canada"] = "Canada"
 df$color[df$country == "Canada"] = "blue"
 
-df$parent2[df$country == "France"] = "France"
-df$color[df$country == "France"] = "red"
-df$parent2[df$country == "Israel"] = "Israel"
-df$color[df$country == "Israel"] = "green"
-df$parent2[df$country == "Canada"] = "Canada"
-df$color[df$country == "Canada"] = "blue"
+df$parent2[df$country == "United Kingdom"] = "United Kingdom"
+df$color[df$country == "United Kingdom"] = "darkred"
+df$parent2[df$country == "Germany"] = "Germany"
+df$color[df$country == "Germany"] = "darkgreen"
+df$parent2[df$country == "Singapore"] = "Singapore"
+df$color[df$country == "Singapore"] = "darkblue"
+
+df$parent2[df$country == "United Kingdom"] = "United Kingdom"
+df$color[df$country == "United Kingdom"] = "darkred"
+df$parent2[df$country == "Germany"] = "Germany"
+df$color[df$country == "Germany"] = "darkgreen"
+df$parent2[df$country == "Singapore"] = "Singapore"
+df$color[df$country == "Singapore"] = "darkblue"
 
 setwd("~/Dropbox/PABalland.github.io/ai-world")
 writeLines(toJSON(df, pretty = TRUE), "modified_top_players.json")
