11971bdf3ad762630f26063f3219cbc4c87ab7a3
Pierre-Alexandre Balland <pierre.alex.balland@gmail.com>
2025-09-14 17:24:42 +0200

    up

---
+++ asg/eciv/ecosystems2/read-files.r
@@ -0,0 +1,17 @@
+# Base URL
+base_url <- "https://www.paballand.com/asg/eciv/"
+
+# Get all files relative to current folder (no ./ in front)
+files <- list.files(path = ".", recursive = TRUE, full.names = FALSE)
+
+# Build URLs
+urls <- paste0(base_url, files)
+
+# Write to a text file
+writeLines(urls, "urls.txt")
+
+# Preview first few
+head(urls)
+
+
+
