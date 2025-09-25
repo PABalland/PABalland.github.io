4f5dcc46d428641a44eeb769cd029cc1e5ebc544
Pierre-Alexandre Balland <pierre.alex.balland@gmail.com>
2025-09-25 20:58:46 +0200

    up

---
+++ asg/eciv/ecosystems/read-files.r
@@ -0,0 +1,18 @@
+setwd("~/Library/CloudStorage/Dropbox/GitHub/PABalland.github.io/asg/eciv/ecosystems2")
+# Base URL
+base_url <- "https://www.paballand.com/asg/eciv/ecosystems2/"
+
+# Get all files relative to current folder (no ./ in front)
+files <- list.files(path = ".", recursive = TRUE, full.names = FALSE)
+
+# Build URLs
+urls <- paste0(base_url, files)
+
+# Write to a text file
+writeLines(urls, "urls.txt")
+
+# Preview first few
+head(urls)
+
+
+
