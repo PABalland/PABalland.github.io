36cf124dbcbc7c98db5b892d241bfdc29be913ca
Pierre-Alexandre Balland <pierre.alex.balland@gmail.com>
2025-12-03 15:01:43 +0100

    up

--- ceps/vario/ecosystems/list.files.R
+++ ceps/vario/ecosystems/list.files.R
@@ -1,3 +1,4 @@
+setwd("~/Library/CloudStorage/Dropbox/GitHub/PABalland.github.io/ceps/vario/ecosystems")
 folder <- setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 files <- list.files(folder, recursive = TRUE, full.names = TRUE)
 files <- files[grepl("\\.html$", files) & !grepl("_archives|_old", files)]
