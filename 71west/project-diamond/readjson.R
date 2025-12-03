5a9b16253eb8c58b4e5f79a7c238eb5f32b4f382
Pierre-Alexandre Balland <pierre.alex.balland@gmail.com>
2025-12-03 11:25:06 +0100

    up

--- 71west/project-diamond/readjson.R
+++
@@ -1,9 +0,0 @@
-library (jsonlite)
-df <- fromJSON("https://www.paballand.com/71west/project-diamond/consumption.json")
-
-df$carbon = df$cost
-
-df2 = toJSON(df)
-write(df2, "consumption.json")
-
-write.csv2(df, "consumption.csv", row.names = F)
