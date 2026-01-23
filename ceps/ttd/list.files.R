d06c719d6333f934cd6535931ee02c97fc8815f1
Pierre-Alexandre Balland <pierre.alex.balland@gmail.com>
2026-01-23 11:12:49 -0600

    up

---
+++ ceps/ttd/list.files.R
@@ -0,0 +1,5 @@
+folder <- setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+files <- list.files(folder, recursive = TRUE, full.names = TRUE)
+files <- files[grepl("\\.html$", files) & !grepl("_archives|_old", files)]
+files <- gsub("^/Users/pierre-alex/Library/CloudStorage/Dropbox/GitHub/PABalland.github.io", "https://www.paballand.com",files)
+writeLines(files, "links.txt")
