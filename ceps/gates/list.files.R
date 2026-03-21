4e892cf0af1f71a4b31422a38d9b31a57c8342b8
Pierre-Alexandre Balland <pierre.alex.balland@gmail.com>
2026-03-21 11:11:43 +0100

    up

---
+++ ceps/gates/list.files.R
@@ -0,0 +1,6 @@
+setwd("~/Library/CloudStorage/Dropbox/GitHub/PABalland.github.io/ceps/gates")
+folder = folder <- "~/Library/CloudStorage/Dropbox/GitHub/PABalland.github.io/ceps/gates"
+files <- list.files(folder, recursive = TRUE, full.names = TRUE)
+files <- files[grepl("\\.html$", files) & !grepl("_archives|_old", files)]
+files <- gsub("^/Users/pierre-alex/Library/CloudStorage/Dropbox/GitHub/PABalland.github.io", "https://www.paballand.com",files)
+writeLines(files, "links.txt")
