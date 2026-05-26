7ad4030e491a4e6ac6e4757a684d7b38e99114c7
Pierre-Alexandre Balland <pierre.alex.balland@gmail.com>
2026-05-26 08:47:48 -0400

    up

--- asg/ron-dep/list.files.R
+++ asg/ron-dep/list.files.R
@@ -1,7 +1,9 @@
+
+
 p <- "ron-dep"
 setwd(paste0("~/Library/CloudStorage/Dropbox/GitHub/PABalland.github.io/asg/", p))
 folder <- paste0("~/Library/CloudStorage/Dropbox/GitHub/PABalland.github.io/asg/", p)
 files <- list.files(folder, recursive = TRUE, full.names = TRUE)
-files <- files[grepl("\\.(html|pdf)$", files) & !grepl("_archives|_old", files)]
+files <- files[grepl("\\.(html|pdf|csv)$", files) & !grepl("_archives|_old", files)]
 files <- gsub("^/Users/pierre-alex/Library/CloudStorage/Dropbox/GitHub/PABalland.github.io", "https://www.paballand.com", files)
 writeLines(files, "links.txt")
