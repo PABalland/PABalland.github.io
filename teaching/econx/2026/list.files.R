087bae11eb8136718bfe41a095ab146879b37cd1
Pierre-Alexandre Balland <pierre.alex.balland@gmail.com>
2026-06-05 18:36:11 +0200

    up

--- teaching/econx/2026/list.files.R
+++ teaching/econx/2026/list.files.R
@@ -1,9 +1,8 @@
 
-
-p <- "ron-dep"
 setwd("~/Library/CloudStorage/Dropbox/GitHub/PABalland.github.io/teaching/econx/2026")
 folder <- setwd("~/Library/CloudStorage/Dropbox/GitHub/PABalland.github.io/teaching/econx/2026")
 files <- list.files(folder, recursive = TRUE, full.names = TRUE)
 files <- files[grepl("\\.(html|pdf|csv)$", files) & !grepl("_archives|_old", files)]
 files <- gsub("^/Users/pierre-alex/Library/CloudStorage/Dropbox/GitHub/PABalland.github.io", "https://www.paballand.com", files)
 writeLines(files, "links.txt")
+
